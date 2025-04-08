abf9350ecf3fce023e986650d7faec672ada3aed
JoaoCostaTI <joao.costapp@outlook.com>
2025-04-08 16:07:21 -0300

    criação inicial dos desafios

---
+++ modulo 1/db002.py
@@ -0,0 +1,5 @@
+dia = input('Informe o dia do seu nascimento: ')
+mes = input('Informe o mes do seu nascimento: ')
+ano = input('Informe o ano do seu nascimento: ')
+
+print(f'Você nasceu em {dia}/{mes}/{ano} . Correto?') 
---
+++ modulo 1/db003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Informe o primeiro numero '))
+n2 = int(input('Informe o segundo numero '))
+soma = n1 + n2
+
+print(f'A soma de {n1} + {n2} = {soma}')
