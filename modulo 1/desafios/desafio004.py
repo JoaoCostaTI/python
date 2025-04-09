e5da8a2db9dc4acf1a398edc78a04394d48389ad
JoaoCostaTI <joao.costapp@outlook.com>
2025-04-09 14:05:58 -0300

    c

---
+++ modulo 1/desafios/desafio004.py
@@ -0,0 +1,7 @@
+algo = input('Digite algo: ')
+
+print(type(algo))
+print(algo.isalpha())
+print(algo.islower())
+print(algo.isnumeric())
+print(algo.isupper())
---
+++ modulo 1/exercicios/ex003.py
@@ -0,0 +1,5 @@
+n1 = input('Informe o primeiro numero ')
+n2 = input('Informe o segundo numero ')
+soma = float(n1) + float(n2)
+
+print(f'A soma de {n1} + {n2} = {soma}')
