a108b743270384267c69735e2a8784ef7375c85a
JoaoCostaTI <joao.costapp@outlook.com>
2025-04-11 16:11:59 -0300

    Update manipulandoStrings.py

--- modulo 1/manipulandoStrings.py
+++ modulo 1/manipulandoStrings.py
@@ -1,14 +1,9 @@
-'''frase = "Curso em Video Python"
+frase = "Curso em Video Python"
 
 print(frase[9::3])
 print(len(frase))
 print(frase.count('o', 0, 13))
 print(frase.find('deo'))
-print(frase.find('Android'))'''
-
-usuario = input('Digite sua senha: ') #senha
-
-while usuario.find('senha') == -1:
-    print('Senha incorreta, tente novamente')
-    usuario = input('Digite sua senha: ')
-print('Senha correta! Pode passar!')
+print(frase.find('Android'))
+frase.replace('Python', 'Android')
+print(frase)
