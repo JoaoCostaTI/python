86f6b6eae6d4895181ce7de63ceeddfb19c5fb58
JoaoCostaTI <joao.costapp@outlook.com>
2025-04-23 16:11:10 -0300

    Update desafio045.py

--- modulo 1/desafios/desafio045.py
+++ modulo 1/desafios/desafio045.py
@@ -18,3 +18,31 @@ elif escolhaMaquina == 'papel' and jogador == 1:
 elif escolhaMaquina == 'tesoura' and jogador == 2:
     print(f'Sua escolha: {jogador} Papel\nEscolha da máquina: {escolhaMaquina} \nMÁQUINA venceu!') 
 else: print(f'EMPATE \nEscolha da máquina: {escolhaMaquina}')
+
+import random
+
+# Mapeamento das escolhas
+opcoes = ['pedra', 'papel', 'tesoura']
+
+# Entrada do jogador
+jogador = int(input('Escolha uma opção \n[1] Pedra\n[2] Papel\n[3] Tesoura\n'))
+
+# Validar entrada
+if jogador not in [1, 2, 3]:
+    print("Escolha inválida! Tente novamente com 1, 2 ou 3.")
+else:
+    escolha_jogador = opcoes[jogador - 1]
+    escolha_maquina = random.choice(opcoes)
+
+    print(f'Sua escolha: {escolha_jogador.capitalize()}')
+    print(f'Escolha da máquina: {escolha_maquina.capitalize()}')
+
+    # Verificando o resultado
+    if escolha_jogador == escolha_maquina:
+        print("EMPATE!")
+    elif (escolha_jogador == 'pedra' and escolha_maquina == 'tesoura') or \
+         (escolha_jogador == 'papel' and escolha_maquina == 'pedra') or \
+         (escolha_jogador == 'tesoura' and escolha_maquina == 'papel'):
+        print("VOCÊ venceu!")
+    else:
+        print("MÁQUINA venceu!")
