f2cde683aa5565adf93351886feccd765b72ae11
JoaoCostaTI <joao.costapp@outlook.com>
2025-04-24 10:59:49 -0300

    exercicios

--- modulo 1/desafios/desafio041.py
+++ modulo 1/desafios/desafio041.py
@@ -1,13 +1,13 @@
 anoNascimento = int(input('Digite o seu ano de nascimento: '))
-anoAtual = 2025
+anoAtual = 2017
 idade = anoAtual - anoNascimento
 
 if idade <= 9:
-    print(f'{idade} Categoria MIRIM!')
+    print(f'Você tem {idade} anos\nCategoria MIRIM!')
 elif idade > 9  and idade <= 14:
-    print(f'{idade} Categoria INFANTIL!')
+    print(f'Você tem {idade} anos\nCategoria INFANTIL!')
 elif idade > 14 and idade <= 19:
-    print(f'{idade} Categoria JUNIOR!')
-elif idade == 20:
-    print(f'{idade} Categoria SENIOR!')
-else: print(f'{idade} Categoria MASTER!')
+    print(f'Você tem {idade} anos\nCategoria JUNIOR!')
+elif idade <= 25:
+    print(f'Você tem {idade} anos\nCategoria SENIOR!')
+else: print(f'Você tem {idade} anos\nCategoria MASTER!')
