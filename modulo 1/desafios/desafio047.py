eb7ef679c3b801768f168af08de0cb82a62e8fda
joao.costapp@outlook.com <joao.costapp@outlook.com>
2025-04-29 16:27:40 -0300

    as

--- modulo 1/desafios/desafio047.py
+++ modulo 1/desafios/desafio047.py
@@ -1,3 +1,9 @@
+'''
 for c in range(1,51):
+
     if c%2 == 0:
-        print(f'{c}')
+        print(f'{c}', end=" ")
+'''
+
+for c in range(0, 51, 2):
+    print(c, end=" ")
--- modulo 1/desafios/desafio048.py
+++ modulo 1/desafios/desafio048.py
@@ -2,5 +2,5 @@ soma = 0
 
 for c in range (1,501):
     if c%2 != 0 and c%3 == 0:
-        soma = soma + c
+        soma += c
 print(soma)
