6dc40241cd4c3c815c96677fa17b884794a7aadc
joao.costapp@outlook.com <joao.costapp@outlook.com>
2025-04-30 14:08:03 -0300

    ex

--- modulo 1/desafios/desafio050.py
+++ modulo 1/desafios/desafio050.py
@@ -6,6 +6,7 @@ for c in range(0,6):
     
     if n % 2 == 0:
         print(f'O numero {n} é par, foi adicionado a soma! ')
-        somaPares = somaPares + n
+        somaPares += n
     else: print(f'O numero {n} é ímpar, não foi adicionado a soma! ')
+    
 print(f'A Soma total dos numeros pares deu: {somaPares}')
