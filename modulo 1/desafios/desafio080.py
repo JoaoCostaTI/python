4009dcfa6d2cf4474e723f1b0c689dbf55b32fa2
joao.costapp@outlook.com <joao.costapp@outlook.com>
2025-05-21 09:08:59 -0300

    Update desafio080.py

--- modulo 1/desafios/desafio080.py
+++ modulo 1/desafios/desafio080.py
@@ -1,16 +1,18 @@
 lista = []
 
-for c in range(5):
-    #Pedindo os numeros
-    n = int(input('Digite um valor: '))
+for c in range (5):
+    n = int(input('Digite um numero: '))
 
     if c == 0 or n > lista[-1]:
         lista.append(n)
+        print('Adicionado no final da lista...')
     else:
-        pos = 0
-        while pos < len(lista):
-            if n <= lista[pos]:
-                lista.insert(pos, n)
+        controle = 0
+        while controle < len(lista):
+            if n <= lista[controle]:
+                lista.insert(controle, n)
+                print(f'Adicionado na posição {controle}')
                 break
-            pos += 1
-print(f'Lista completa: {lista}')
+            controle += 1
+
+print(f'Lista completa ordenada: {lista}')
