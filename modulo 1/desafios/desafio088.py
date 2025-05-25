87de4109c43f122521da81cc0d8000285f7eff81
JoaoCostaTI <joao.costapp@outlook.com>
2025-05-25 10:45:17 -0300

    as

--- modulo 1/desafios/desafio088.py
+++ modulo 1/desafios/desafio088.py
@@ -8,14 +8,27 @@ jogos = int(input('Quantos palpites de jogos gerar? '))
 
 for qtdJogos in range(jogos):
     #Preenchendo os 6 numeros
-    for jogo in range(6):
-        palpites.insert(qtdJogos, randint(1,60))
+    contador = 0
+    for jogo in range(2):
+        numeroMega = randint(1,60)
+        if contador == 0:
+            palpites.append(numeroMega)
+            contador += 1
+        else:
+            while len(palpites) < 6:
+                numeroMega = randint(1,60)
+                if numeroMega not in palpites:
+                    palpites.append(numeroMega)
 
     #Adicionando os palpites na lista final
     palpitesFinais.insert(qtdJogos, palpites[:])
 
     #Limpando lista palpites
     palpites.clear()
+
+#ordenando as listas
+for sublista in palpitesFinais:
+    sublista.sort()
 
 #Exibindo os resultados
 linhaJogo = 1
