cb4c71e74179aeb5cd41b8a2ae6d03b700b6169a
JoaoCostaTI <joao.costapp@outlook.com>
2025-05-27 09:01:23 -0300

    Update desafio089.py

--- modulo 1/desafios/desafio089.py
+++ modulo 1/desafios/desafio089.py
@@ -1,21 +1,36 @@
+
 dados = []
-listaFinal = []
+alunos = []
 
 while True:
-    nome = str(input('Nome: '))
-    dados.append(nome)
-    
-    for nota in range(2):
-        notas = float(input(f'Nota {nota+1}: '))
-        dados.append(notas)
+    #Coletando os dados:
+    dados.append(str(input('Nome: ')))
+    dados.append(float(input('Nota 1: ')))
+    dados.append(float(input('Nota 2: ')))
+    media = (dados[1] + dados[2]) / 2
+    dados.append(media)
 
-    listaFinal = dados[:]
-
+    #Colocando os dados em alunos na lista final
+    alunos.append(dados[:])
     dados.clear()
 
     op = str(input('Deseja continuar? [S/N]: ')).upper().strip()
 
-    if op in 'nN':
+    if op in 'N':
         break
+print(15*'-=')
+print(f'{'No.':<4}{'NOME':<15}{'MÉDIA':<6}')
+print(10*'---')
+for indice, inf in enumerate(alunos):
+    print(f'{indice:<4}{inf[0]:<15}{inf[3]:>6.1f}')
 
-print(listaFinal)
+while True:
+    chave = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+
+    for indice, inf in enumerate(alunos):
+        if indice == chave:
+            print(f'Notas de {inf[0]} são {inf[1], inf[2]}')
+    print(10*'---')
+    if chave == 999:
+        print('Saindo do programa...')
+        break
