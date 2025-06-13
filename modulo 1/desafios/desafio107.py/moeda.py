043fefcce60faea6bf2cbfea226b77da73f6f4ce
JoaoCostaTI <joao.costapp@outlook.com>
2025-06-13 11:26:58 -0300

    as

---
+++ modulo 1/desafios/desafio107.py/moeda.py
@@ -0,0 +1,15 @@
+def aumentar(n, porcentagem = 10):
+    resultado10 = (n*porcentagem) / 100
+    resultadof = n + resultado10
+    return resultadof
+
+def diminuir(n, porcentagem = 13):
+    resultado13 = (n*porcentagem) / 100
+    resultadof = n - resultado13
+    return resultadof
+
+def dobro(n):
+    return n * 2
+
+def metade(n):
+    return n / 2
--- modulo 1/desafios/refinamentos.py
+++ modulo 1/desafios/refinamentos.py
@@ -1,7 +1,5 @@
-numero = input('Digite algo: ')
-
-if numero.isnumeric():
-    numero = int(numero)
-    print('Sim é um numero!!!')
-if isinstance(numero, str):
-    print('Sim é string!!!')
+f = 1
+n = 5
+for c in range(1, n+1):
+    f *= c
+print(f)
